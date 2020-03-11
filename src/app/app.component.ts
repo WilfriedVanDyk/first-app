a2cb8136645b7d22564c33d3621e57f45d9c87eb
WilfriedVanDyk <wilfriedvandyk@gmail.com>
2020-03-11 16:06:04 +0100

    counterService werkt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CounterService } from './counter.service';
+import { Subject, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,20 +9,25 @@ import { CounterService } from './counter.service';
 })
 
 export class AppComponent {
+  title = 'first-app';
 
   secondsLocal: number = 60;
-  
-  constructor(counterService:CounterService) {
-counterService.secondsSubject.subscribe((argumentSeconds) => {
-  this.secondsLocal  = argumentSeconds;
-});
+ //secondsSubj: Observable<number>;
+//  tijdResetLocal;
+service: CounterService;
+
+  constructor(counterService: CounterService) {
+    this.service=counterService;
+      // counterService.secondsSubject.subscribe((argumentSeconds) => {
+      //   this.secondsLocal = argumentSeconds;
+     //});
+
+    //  this.tijdResetLocal=counterService.tijdReset();
   }
 
 
 
-
-  title = 'first-app';
-
+////////////////////////////////////////////////////////////////////////////////
   tabelGokGetallen = [
     { gokPoging: 1, getal: null },
     { gokPoging: 2, getal: null },
@@ -50,52 +56,69 @@ counterService.secondsSubject.subscribe((argumentSeconds) => {
   gewonnenBoodschap: string;
   gewonnen: boolean = false;
   verloren: boolean = false;
+  eersteGok: boolean = true;
   cijferHint: string = "Het nieuwe getal is te hoog of te laag. Kies opnieuw."
 
+  getalOK = () => {
+    //als het de eerste gok is terug naar beginwaarde: anders zo laten
+    if(this.secondsLocal<= 60 && this.eersteGok){
+      this.service.tijdReset();
+    this.eersteGok=false;
+    }
+    
+    this.service.secondsSubject.subscribe((argumentSeconds) => {
+        this.secondsLocal = argumentSeconds;
+     });
+    
 
-  getalOK() {
+
     if (this.value === this.randomNummer) {
       this.tabelGokGetallen[this.aantalGokkenGedaan - 1].getal = this.value;
       this.gewonnenBoodschap = "Proficiat! Dit is het juiste getal!! klik op de Herbegin! button om opnieuw te spelen";
       this.gewonnen = true;
-      this.tabelGokGetallen.length = this.aantalGokkenGedaan;
-     
+      this.tabelGokGetallen.length = this.aantalGokkenGedaan;     
+    }
+    else if(this.secondsLocal<=0){
+      this.verlorenBoodschap = "je tijd is op. Jammer, je bent verloren! " +
+      "het juiste getal was "
+    this.verloren = true;
+    this.tabelGokGetallen.length = this.aantalGokkenGedaan;
     }
 
-    else if (this.aantalGokken <=1  && this.value!=null) {
+    else if (this.aantalGokken <= 1 && this.value != null ) {
       this.tabelGokGetallen[this.aantalGokkenGedaan - 1].getal = this.value;
-      this.verlorenBoodschap="Het aantal beurten is op. Jammer, je bent verloren! "+
-      "het juiste getal was "
+      this.verlorenBoodschap = "Het aantal beurten is op. Jammer, je bent verloren! " +
+        "het juiste getal was "
       this.verloren = true;
       this.tabelGokGetallen.length = this.aantalGokkenGedaan;
     }
 
-   else if (!this.gewonnen && !this.verloren && this.value!=null && (this.value > 100 || this.value <= 0)) {
-      this.value=null;
-      this.hint="";
+    else if (!this.gewonnen && !this.verloren && this.value != null && (this.value > 100 || this.value <= 0)) {
+      this.value = null;
+      this.hint = "";
     }
 
-    
-    else if (this.value < this.randomNummer && this.gewonnen ==false && this.value!=null) {
+
+    else if (this.value < this.randomNummer && this.gewonnen == false && this.value != null) {
       this.hint = "het gegeven getal is te laag. ";
       this.aantalGokken--;
       this.aantalGokkenGedaan++;
       this.itemToevoegenAanTabel();
-      this.value=null;
+      this.value = null;
 
     }
-    else if (this.value > this.randomNummer && this.gewonnen ==false && this.value!=null) {
+    else if (this.value > this.randomNummer && this.gewonnen == false && this.value != null) {
       this.hint = "het gegeven getal is te hoog.";
       this.aantalGokken--;
       this.aantalGokkenGedaan++;
       this.itemToevoegenAanTabel();
-      this.value=null;
+      this.value = null;
     }
-    
   }
 
-  herbeginSpel() {
+  herbeginSpel = () => {    
 
+    this.service.tijdReset();
 
     this.randomNummer = Math.floor((Math.random() * 100) + 1);
     this.aantalGokken = 10;
@@ -104,7 +127,8 @@ counterService.secondsSubject.subscribe((argumentSeconds) => {
     this.verloren = false;
     this.nullBoodschap = "Geef een nieuw nummer in.";
     this.value = null;
-    this.hint="";
+    this.hint = "";
+    this.eersteGok = true;
     this.tabelGokGetallen = [
       { gokPoging: 1, getal: null },
       { gokPoging: 2, getal: null },
@@ -118,8 +142,8 @@ counterService.secondsSubject.subscribe((argumentSeconds) => {
       { gokPoging: 10, getal: null },
     ];
   }
-  itemToevoegenAanTabel() {    
-     this.tabelGokGetallen[this.aantalGokkenGedaan - 2].getal = this.value;
+  itemToevoegenAanTabel = () => {
+    this.tabelGokGetallen[this.aantalGokkenGedaan - 2].getal = this.value;
   }
 
 }
--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -1,29 +1,44 @@
 import { Injectable } from '@angular/core';
-import { Subject} from 'rxjs';
+import { Subject } from 'rxjs';
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterService {
 
-
-  private counter = 60;
-
+private counter = 60;
   secondsSubject: Subject<number> = new Subject<number>();
-  seconds = this.secondsSubject.asObservable(); 
-  //wordt hier niet gebuikt maar in de typescript file waarin je dit subject gebruikt !!!
-  //maar wordt hier  wel aangemaakjt.
+  seconds = this.secondsSubject.asObservable();
+  //wordt hier niet gebruikt maar in de typescript file waarin je dit subject gebruikt !!!
+  //maar wordt hier  wel aangemaakt.
+  
 
-   constructor() { 
-    setInterval( () => {
-      this.counter -= 1;
-      this.secondsSubject.next(this.counter);
-      //evaluate
-    }, 1000);    
+  tijdReset = () => {
+   this.counter=60;    
   }
-// evaluate( () => {
 
-// })))
-// window.location.reload
-// window.alert
+  interval = setInterval(() => {
+    if(this.counter>=1){
+    this.counter -= 1;}
+    this.secondsSubject.next(this.counter);
+  }, 1000);
+
+  constructor() {
+    // setInterval(() => {
+    //   this.counter -= 1;
+    //   this.secondsSubject.next(this.counter);
+    //   this.tijdisOp();
+    //   //console.log(this.seconds);
+    // }, 1000);
+
+    //this.interval;
+
+  }
+
+
+  // window.location.reload
+  // window.alert
 }
